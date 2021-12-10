0c94af4019538e1b049c81ed5802f186f24de34c
Abraham <abrahamag93@gmail.com>
2021-12-10 14:11:38 -0600

    Inicializar Repositorio

---
+++ app/Http/Controllers/OrdenController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Orden;
+use App\Http\Requests\StoreOrdenRequest;
+use App\Http\Requests\UpdateOrdenRequest;
+
+class OrdenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOrdenRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOrdenRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Orden  $orden
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Orden $orden)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Orden  $orden
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Orden $orden)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOrdenRequest  $request
+     * @param  \App\Models\Orden  $orden
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOrdenRequest $request, Orden $orden)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Orden  $orden
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Orden $orden)
+    {
+        //
+    }
+}
